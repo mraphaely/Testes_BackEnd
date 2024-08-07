ec99ed659d2c67a924fac35dc6a0dd0a99e4583b
mraphaely <raphaelymaryana@gmai.com>
2024-08-07 18:27:09 -0300

    add ex e aula 02, mod em aula 01

--- Aula_01/index.spec.js
+++ Aula_01/index.spec.js
@@ -7,3 +7,27 @@ Terminal jest para testes em JS:
    4. criar um arquivo chamado index.spec.js ou index.test.js
    5. npm run test
 */
+
+function soma( a, b ){
+    return a + b;
+}
+
+test("Soma 1 + 4 e retorna 5", () => {
+    expect(soma(1, 4)).toBe(5);
+    expect(soma(2, 3)).toBe(5);
+    expect(soma(2.5, 2.5)).toEqual(5);
+});
+
+test("Verifica se 18 é maior que", () => {
+    const numero = 18
+    expect(20).toBeGreaterThan(numero);
+    expect(18).toBeGreaterThanOrEqual(numero);
+});
+
+// Matchers para string
+
+test('Procura da letra I', () => {
+    const nome = 'Mary'
+    expect(nome).toMatch(/M/);
+    expect(nome).not.toMatch(/N/);
+})
---
+++ Exercicios/Ex.test.js
@@ -0,0 +1,33 @@
+/*
+1- Crie uma função que calcula a área do quadrado, que recebe um número e retorna o quadrado desse número.
+Realize o teste utilizando os matchers toBe(), toBeGreaterThan(), toBeLessThan().
+*/
+// square.js
+function AreaQuadrado(valor) {
+    if (valor < 0) {
+        throw new Error("O lado do quadrado não pode ser negativo");
+    }
+    return valor ** 2;
+}
+
+test("Retorna o quadrado do número", () => {
+    expect(AreaQuadrado(4)).toBe(16);
+});
+
+test("Retorna um valor maior que o número", () => {
+    expect(AreaQuadrado(4)).toBeGreaterThan(4);
+});
+
+test("Retorna um valor menor que o dobro do número ao quadrado", () => {
+    expect(AreaQuadrado(4)).toBeLessThan(4 ** 3);
+});
+
+test("Erro se o lado for negativo", () => {
+    expect(() => AreaQuadrado(-4)).toThrowError(
+        "O lado do quadrado não pode ser negativo"
+    );
+});
+/*
+2- Crie uma função chamada 'media' que recebe três valores e retorna a média desses valores.
+Crie um teste usando o método toBeCloseTo() para validar essa função.
+*/
