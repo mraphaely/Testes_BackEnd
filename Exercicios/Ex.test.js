ead18167663232b1cd4ffd3ca12b66f9c64ec65e
mraphaely <raphaelymaryana@gmai.com>
2024-08-07 18:27:21 -0300

    add tests

--- Exercicios/Ex.test.js
+++ Exercicios/Ex.test.js
@@ -2,10 +2,9 @@
 1- Crie uma função que calcula a área do quadrado, que recebe um número e retorna o quadrado desse número.
 Realize o teste utilizando os matchers toBe(), toBeGreaterThan(), toBeLessThan().
 */
-// square.js
 function AreaQuadrado(valor) {
     if (valor < 0) {
-        throw new Error("O lado do quadrado não pode ser negativo");
+        throw new Error("Não pode ser negativo");
     }
     return valor ** 2;
 }
@@ -24,10 +23,16 @@ test("Retorna um valor menor que o dobro do número ao quadrado", () => {
 
 test("Erro se o lado for negativo", () => {
     expect(() => AreaQuadrado(-4)).toThrowError(
-        "O lado do quadrado não pode ser negativo"
+        "O número não pode ser negativo"
     );
 });
 /*
 2- Crie uma função chamada 'media' que recebe três valores e retorna a média desses valores.
 Crie um teste usando o método toBeCloseTo() para validar essa função.
 */
+function media(a, b, c) {
+    return (a + b + c) / 3;
+}
+test("Retorna a média", () => {
+    expect(media(6, 7, 8)).toBeCloseTo(7);
+})
